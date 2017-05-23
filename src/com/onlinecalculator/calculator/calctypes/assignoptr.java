ebea0d7591d0e02b792d3dfc1e12285533e7edb0
istar <istar@192.168.100.22>
2017-05-23 19:53:10 +0530

    assignoptr by bhavani

---
+++ src/com/onlinecalculator/calculator/calctypes/assignoptr.java
@@ -0,0 +1,18 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.Scanner;
+public class assignoptr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a,b;
+Scanner s=new Scanner(System.in);
+System.out.println("enter first number");
+a=s.nextInt();
+System.out.println("enter second number");
+b=s.nextInt();
+System.out.println("a+=b"+(a+=b));
+System.out.println("a-=b"+(a-=b));
+System.out.println("a*=b"+(a*=b));
+System.out.println("a%=b"+(a%=b));
+}
+}
